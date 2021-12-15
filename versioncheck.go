d81705c2c05326643b0141ae7a2aefad78a4077f
bakingbacon <bangbangtaco@protonmail.com>
2021-12-15 17:57:31 +0000

    added simple anonymous stats

--- versioncheck.go
+++ versioncheck.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"runtime"
 	"time"
 
 	"github.com/pkg/errors"
@@ -16,6 +19,7 @@ import (
 
 const (
 	VERSION_URL = "https://bakingbacon.github.io/bakinbacon/version.json"
+	STATS_URL   = "https://bakinbacon.io/stats.php"
 )
 
 var (
@@ -33,10 +37,12 @@ type Version struct {
 
 func (bb *BakinBacon) RunVersionCheck() {
 
-	// Check every 12hrs
-	ticker := time.NewTicker(24 * time.Hour)
+	// Check every 8hrs
+	ticker := time.NewTicker(8 * time.Hour)
 
 	for {
+
+		bb.submitAnonymousStats()
 
 		versions := Versions{}
 
@@ -77,4 +83,35 @@ func (bb *BakinBacon) RunVersionCheck() {
 		// wait here for next iteration
 		<-ticker.C
 	}
+}
+
+func (bb *BakinBacon) submitAnonymousStats() {
+
+	// Get PKH of baker
+	_, pkh, err := bb.Signer.GetPublicKey()
+	if err != nil {
+		log.WithError(err).Error("Unable to get PKH for stats")
+		return
+	}
+
+	// create hash of PKH to anonymize the info
+	uuid := fmt.Sprintf("%x", md5.Sum([]byte(pkh)))
+
+	// json of stats
+	stats, _ := json.Marshal(map[string]string{
+		"uuid":  uuid,
+		"os":    fmt.Sprintf("%s-%s", runtime.GOOS, runtime.GOARCH),
+		"chash": commitHash,
+		"bbver": version,
+	})
+
+	statsPostBody := bytes.NewBuffer(stats)
+	resp, err := http.Post(STATS_URL, "application/json", statsPostBody)
+	if err != nil {
+		log.WithError(err).Error("Unable to post stats")
+		return
+	}
+	defer resp.Body.Close()
+
+	log.Debug("Posted anonymous stats")
 }
